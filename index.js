7e09bdee29a14b03807827fc770e2da8901d0190
rathesh77 <rathesh.charles@outlook.fr>
2021-03-21 00:12:33 +0100

    update (code is mess...)

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ class Noeud {
      * @param {Number} distanceParcouru 
      * @param {Array<Noeud>} stationsAdjacentes 
      */
-    constructor(distanceParcouru, stationsAdjacentes) {
+    constructor(distanceParcouru, stationsAdjacentes, stationName) {
         this.distanceParcouru = distanceParcouru;
         this.stationsAdjacentes = stationsAdjacentes;
+        this.stationName = stationName
     }
     /**
      * 
@@ -19,7 +20,7 @@ class Noeud {
 }
 function travel(noeud) {
     let clone = noeud
-    console.log(clone.distanceParcouru)
+    console.log(clone.stationName)
     if (!clone.stationsAdjacentes.length)
         return
     travel(clone.stationsAdjacentes[Math.floor(Math.random() * clone.stationsAdjacentes.length)])
@@ -38,40 +39,108 @@ function getMin(noeud, min, depth) {
     return newMin
 }
 
-function shortestPath(departure, destination) {
+function shortestPath(departure, destination, cache) {
 
-    let min = getMin(departure, null, 1);
-    console.log(min)
-    let newStationsAdjacentes = getStationsAdjacentesExceptMin(min, departure.stationsAdjacentes)
-    console.log(newStationsAdjacentes)
+    //console.log(cache)
+    if (!departure)
+        return "cette station n'existe pas"
+    let from = departure
+    if (from.stationName == destination)
+        return from
+    cache = detectDup(from.stationsAdjacentes,cache)
+    from.stationsAdjacentes = everyStationComeFrom(from)
+    from.stationsAdjacentes = from.stationsAdjacentes.concat(cache)
+    
+    let min = getMini(from.stationsAdjacentes);
+    
+        min.distanceParcouru += min.from.distanceParcouru
+        //console.log("-----------------------")
+    if (from.stationName == "F")
+    console.log(from.stationsAdjacentes)
+    let newStationsAdjacentes = getStationsAdjacentesExceptMin(min, from.stationsAdjacentes)
+    return shortestPath(min, destination, newStationsAdjacentes)
 }
-
+function detectDup(from, cache) {
+    for (let i = 0; i < cache.length; i++) {
+        for (let j = 0; j < from.length; j++) {
+            if (from[j].stationName == cache[i].stationName)
+            {
+               // console.log(from[j].distanceParcouru,cache[i].distanceParcouru)
+                if ( from[j].distanceParcouru < cache[i].distanceParcouru)
+                {
+                    cache[i].distanceParcouru = from[j].distanceParcouru
+                }
+            }
+    }
+    }
+    return cache
+}
+function getMini(arr) {
+    let min = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].distanceParcouru < min.distanceParcouru) {
+            min = arr[i]
+        }
+    }
+    return min
+}
+function everyStationComeFrom(from) {
+    let clone = from
+    for (let i = 0; i < clone.stationsAdjacentes.length; i++) {
+        clone.stationsAdjacentes[i].from = from;
+    }
+    return clone.stationsAdjacentes
+}
 function getStationsAdjacentesExceptMin(min, stations) {
 
-    return stations.filter(s => s.distanceParcouru != min.distanceParcouru);
+    let p = stations.filter(s => s.distanceParcouru != min.distanceParcouru);
+    //console.log(p);
+    return p
 }
 
+function graphFromArrayOfNode(arr) {
+    if (!arr.length)
+        return
+    //console.log(arr)
+    return new Noeud(arr[0].distanceParcouru, arr.slice(1), null)
+}
+/*
+let root = new Noeud(788, [], "a")
+let n1 = new Noeud(728, [], "b")
+let n2 = new Noeud(578, [], "c")
+let n3 = new Noeud(698, [], "d")
 
-let root = new Noeud(788, [])
-let n1 = new Noeud(728, [])
-let n2 = new Noeud(578, [])
-let n3 = new Noeud(698, [])
+n1.addStationAdjacente(new Noeud(456, [new Noeud(459, [], "f"), new Noeud(698, [], "g"), new Noeud(700, [], "h")] , "e"))
+n1.addStationAdjacente(new Noeud(485, [new Noeud(425, [], "j"), new Noeud(856, [], "k"), new Noeud(264, [], "l")], "i"))
+n1.addStationAdjacente(new Noeud(369, [new Noeud(400, [], "n"), new Noeud(320, [new Noeud(296, [], "o"), new Noeud(296, [], "p")], "qwesh"), new Noeud(489, [])], "m"))
 
-n1.addStationAdjacente(new Noeud(456, [new Noeud(459, []), new Noeud(698, []), new Noeud(700, [])]))
-n1.addStationAdjacente(new Noeud(485, [new Noeud(425, []), new Noeud(856, []), new Noeud(264, [])]))
-n1.addStationAdjacente(new Noeud(369, [new Noeud(400, []), new Noeud(320, [new Noeud(296, []), new Noeud(296, [])]), new Noeud(489, [])]))
+n2.addStationAdjacente(new Noeud(523, [new Noeud(300, [], "r"), new Noeud(278, [], "s"), new Noeud(264, [], "t")], "q"))
+n2.addStationAdjacente(new Noeud(444, [new Noeud(268, [], "v"), new Noeud(639, [], "w"), new Noeud(221, [], "x")], "u"))
+n2.addStationAdjacente(new Noeud(237, [new Noeud(298, [], "z"), new Noeud(666, [new Noeud(547, [], "aa"), new Noeud(852, [], "ab")], "ac"), new Noeud(456, [], "ad")], "y"))
 
-n2.addStationAdjacente(new Noeud(523, [new Noeud(300, []), new Noeud(278, []), new Noeud(264, [])]))
-n2.addStationAdjacente(new Noeud(444, [new Noeud(268, []), new Noeud(639, []), new Noeud(221, [])]))
-n2.addStationAdjacente(new Noeud(237, [new Noeud(298, []), new Noeud(666, [new Noeud(547, []), new Noeud(852, [])]), new Noeud(456, [])]))
+n3.addStationAdjacente(new Noeud(894, [new Noeud(447, [], "af"), new Noeud(999, [], "ag"), new Noeud(589, [], "ah")], "ae"))
+n3.addStationAdjacente(new Noeud(785, [new Noeud(288, [], "aj"), new Noeud(282, [], "ak"), new Noeud(247, [], "al")], "ai"))
+n3.addStationAdjacente(new Noeud(547, [new Noeud(457, [], "an"), new Noeud(563, [new Noeud(389, [], "ao"), new Noeud(931, [], "ap")],"az"), new Noeud(546, [], "aq")], "am"))
+*/
 
-n3.addStationAdjacente(new Noeud(894, [new Noeud(447, []), new Noeud(999, []), new Noeud(589, [])]))
-n3.addStationAdjacente(new Noeud(785, [new Noeud(288, []), new Noeud(282, []), new Noeud(247, [])]))
-n3.addStationAdjacente(new Noeud(547, [new Noeud(457, []), new Noeud(563, [new Noeud(389, []), new Noeud(931, [])]), new Noeud(546, [])]))
+let root = new Noeud(0, [], "A")
+let n1 = new Noeud(85, [], "B")
+let n2 = new Noeud(217, [], "C")
+let n3 = new Noeud(173, [], "E")
+
+n1.addStationAdjacente(new Noeud(80, [new Noeud(250, [new Noeud(84, [], "J")], "I")], "F"))
+n2.addStationAdjacente(new Noeud(186, [], "G"))
+n2.addStationAdjacente(new Noeud(103, [new Noeud(183, [], "D"), new Noeud(167, [], "J")], "H"))
+
+n3.addStationAdjacente(new Noeud(502, [], "J"))
 
 root.addStationAdjacente([n1, n2, n3])
 
-//console.log(getMin(root, null, 200))
+//console.log(getMin(root, null, 1))
 //travel(root)
+let dest = shortestPath(root, "J", [])
+console.log(dest.stationName)
+while ((dest = dest.from) != null) {
+    console.log(dest.stationName)
 
-shortestPath(root, null)
+}
